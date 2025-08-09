6c93e8c8b0e72776a8032695080205717263125d
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-08-09 10:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    // Prevent form from submitting and reloading page
+    event.preventDefault();
+
+    // Get radius value from input
+    let radius = parseFloat(document.getElementById('radius').value);
+
+    // Calculate volume if valid, else NaN
+    let volume;
+    if (isNaN(radius) || radius < 0) {
+        volume
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
